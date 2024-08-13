5384e88576e7706b2c97ce579259aa2c8bc88aa4
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-13 13:23:14 -0400

    Create Validations for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: %w(Fiction Non-Fiction)
+  validate :required_phrase
+
+  def required_phrase
+    regex = /Won't Believe|Secret|Top \d+|Guess/
+    unless regex.match?(title)
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
